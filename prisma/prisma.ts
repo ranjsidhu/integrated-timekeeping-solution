c1e522fed1c670a6af4057b87877694b11dbf0a1
Ranjeet Sidhu <ranjeetsidhu7433@gmail.com>
2025-12-01 11:48:44 +0000

    ci(prisma): fix unit tests

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,8 +1,13 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
+const connString =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL
+    : process.env.LOCAL_DATABASE_URL;
+
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: connString,
 });
 
 const prisma = new PrismaClient({ adapter });
